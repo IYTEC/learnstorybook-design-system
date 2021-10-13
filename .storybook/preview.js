39957ff839b971d294172aabfce651862be7e02e
Ikechi <kechieleazar@gmail.com>
2021-10-13 10:41:56 +0100

    Storybook deployment with GitHub action

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { GlobalStyle } from '../src/shared/global';
+import React from 'react';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+   Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
